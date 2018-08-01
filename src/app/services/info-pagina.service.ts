269af034f030e24d1ad069b24d9b02429adaeb97
Estefania <tefajarita@gmail.com>
2018-08-01 18:30:26 -0500

    Parte 1 sesión 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+  info: any;
+  cargada = false;
+  constructor(private http: HttpClient) {
+    console.log('info pagina cargada');
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( resp => {
+      this.cargada = true;
+      this.info = resp;
+      console.log(resp);
+    });
+   }
+}
