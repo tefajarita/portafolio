c97a58f7ae08f3879b39b4419a363fa82ab112df
Estefania <tefajarita@gmail.com>
2018-08-02 09:03:19 -0500

    Fin de la sección 4

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  info: any;
+  info: InfoPagina;
   cargada = false;
   constructor(private http: HttpClient) {
     console.log('info pagina cargada');
